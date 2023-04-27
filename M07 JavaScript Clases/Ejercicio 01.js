2ee48a4386a4e3b60249579e2d91edb13105c5b5
PabloELopezH <pexelh16957@gmail.com>
2023-04-27 16:00:22 -0300

    Ejercicios M07

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,7 +10,7 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
-   /*function Usuario(usuario, nombre, email, password) {
+   /* function Usuario(usuario, nombre, email, password) {
       this.usuario = usuario;
       this.nombre = nombre;
       this.email = email;
@@ -22,6 +22,7 @@ function crearUsuario() {
 return Usuario;
 }
 */
+
 class Usuario {
    constructor(usuario, nombre, email, password) {
      this.usuario = usuario;
